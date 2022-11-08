36aef3e35c74c816d21f9dba72c698bade78c9b9
grace manzon <86327895+gracemanzon@users.noreply.github.com>
2022-11-08 17:18:23 -0500

    add artists, songs and genres to snapshot show

--- src/SnapshotShow.jsx
+++ src/SnapshotShow.jsx
@@ -20,6 +20,25 @@ export function SnapshotShow() {
     <div>
       <h1>Snapshot Show Action</h1>
       <h2>{snapshot.title}</h2>
+      <p>
+        {snapshot.start_date} - {snapshot.end_date}
+      </p>
+      <p>{snapshot.user_name}</p>
+      <div>
+        {snapshot.artists?.map((artist) => (
+          <p>{artist.name}</p>
+        ))}
+      </div>
+      <div>
+        {snapshot.songs?.map((song) => (
+          <p>{song.title}</p>
+        ))}
+      </div>
+      <div>
+        {snapshot.genres?.map((genre) => (
+          <p>{genre.title}</p>
+        ))}
+      </div>
     </div>
   );
 }
